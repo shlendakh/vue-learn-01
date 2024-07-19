1f13c1b3be6c6a3dc3e6159827e2a4a922fbb1cd
Paweł Szlendak <kontakt@pawelszlendak.pl>
2024-07-20 01:15:09 +0200

    lessson2

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,13 @@
 <script setup>
-import { ref } from "vue";
-const name = ref('John Doe');
-const status = ref(true);
-const tasks = ref(['Task 1', 'Task 2', 'Task 3']);
-const link = ref('test.com');
-
-const toggleStatus = () => {
-  status.value = !status.value
-}
-
-const newTask = ref('');
-
-
+import Navbar from "./components/Navbar.vue";
+import Hero from "./components/Hero.vue";
+import HomeCards from "./components/HomeCards.vue";
+import JobListings from "./components/JobListings.vue";
 </script>
 
 <template>
-  <h1>{{ name }}</h1>
-  <p v-if="status">User is active</p>
-
-  <form @submit.prevent="addTask">
-    <label for="newTask">Add Task</label>
-    <input type="text" id="newTask" name="newTask" v-model="newTask">
-    <button type="submit">Submit</button>
-  </form>
-
-  <h3>Tasks</h3>
-  <ul>
-    <li v-for="task in tasks" :key="task">{{ task }}</li>
-  </ul>
-  <a :href="link">Link</a>
-
-  <button @click="toggleStatus">Change status</button>
+  <Navbar />
+  <Hero title="Test title" subtitle="Test subtitle" />
+  <HomeCards />
+  <JobListings :limit="3" :show-button="false" />
 </template>
-
-<style scoped></style>
